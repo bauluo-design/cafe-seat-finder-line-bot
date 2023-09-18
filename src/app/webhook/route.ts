0b1cbb84134ac99b5041cbcffbf8983b0d680792
brianliu <hong86323@gmail.com>
2023-09-19 03:25:26 +0800

    feat: add dummy webhook handler

--- src/app/webhook/route.ts
+++ src/app/webhook/route.ts
@@ -1,1 +1,13 @@
-export async function POST(request: Request) {}
+import signatureValidator from "../../ext/line/validator";
+
+export async function POST(request: Request): Promise<Response> {
+  if (
+    !signatureValidator(
+      await request.text(),
+      request.headers.get("x-line-signature") || "",
+    )
+  ) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  return new Response("Hello worker!", { status: 200 });
+}
