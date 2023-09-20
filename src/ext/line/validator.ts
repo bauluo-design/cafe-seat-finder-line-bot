748d215eaad4aa38b9c25a5a3e45b4ae5a671f7f
brianliu <hong86323@gmail.com>
2023-09-21 02:26:21 +0800

    refactor: dependency injection

--- src/ext/line/validator.ts
+++ src/ext/line/validator.ts
@@ -1,20 +1,26 @@
 import * as line from "@line/bot-sdk";
-import secrets from "./config";
+import lineSecrets from "./config";
 import { IS_LOCAL } from "../../config/env";
 
 /**
  * Validate the Line webhook request signature.
  *
  * If in local testing mode, always return true.
+ *
  * @param body: the content of the request from Line webhook.
  * @param signature: the x-line-signature header of the request from Line webhook.
+ * @param isLocal: whether the server is running in local testing mode.
+ * @param channelSecret: Line channel secret used to verify the signature.
  */
 export default function signatureValidator(
   body: string,
   signature: string,
+  // The following two parameters are for testing purpose.
+  isLocal: boolean = IS_LOCAL,
+  channelSecret: string = lineSecrets.channelSecret,
 ): boolean {
-  if (IS_LOCAL) {
+  if (isLocal) {
     return true;
   }
-  return line.validateSignature(body, secrets.channelSecret, signature);
+  return line.validateSignature(body, channelSecret, signature);
 }
